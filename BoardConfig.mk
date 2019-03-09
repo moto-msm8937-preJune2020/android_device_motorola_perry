7e30dd5200a661abf2227c1fa34a8a7196d7dd1d
AsD Monio <asdmonio@gmail.com>
2019-03-09 19:49:24 +0100

    montana: Switch to enforcing SELinux
    Change-Id: Id120a92263a1605ff93162fbd20d32fb1916c404

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ TARGET_OTA_ASSERT_DEVICE := montana,montana_retail
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
 # Kernel
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_LZ4C_DT := true
 TARGET_KERNEL_CONFIG := montana_defconfig
 
