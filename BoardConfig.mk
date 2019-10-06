6fc40014e0c1dba361f6478d5f632dc7c8c8b288
Jarl-Penguin <jarlpenguin@outlook.com>
2019-10-07 01:11:22 +0300

    montana: set vendor security patch level
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,3 +52,6 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # SELinux
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
+# Vendor security patch
+VENDOR_SECURITY_PATCH := 2019-02-01
